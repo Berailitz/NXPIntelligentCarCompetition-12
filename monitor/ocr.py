da389356e8ab908154afccfab60502856d331e57
graphite <admin@ohhere.xyz>
2018-07-30 22:41:33 +0800

    Split numbers; Improve THRESHHOLD_GRAY_MAIN; Use light; TODO: adjust window size;

--- monitor/ocr.py
+++ monitor/ocr.py
@@ -48,21 +48,16 @@ class OCRHandle(object):
         return raw_word_img[y:y+h, x:x+w]
 
     def recognize_number(self, img):
-        img_m = cv2.resize(img, (100, 120))
+        # img_m = cv2.resize(img, (100, 120))
         number = pytesseract.image_to_string(
-            img_m, config='-c tessedit_char_whitelist=0123456789 -psm 10')
+            img, config='-c tessedit_char_whitelist=0123456789 -psm 10')
         return number
-
-    def is_in_image(self, image, x, y) -> bool:
-        width = image.shape[1]
-        height = image.shape[0]
-        return x > 0 and x < width and y > 0 and y < height
 
     @staticmethod
     def is_rect_valid(dot_list: list) -> bool:
         """ dot_list: sorted [(x, y)]
         """
-        MAX_RATIO = 4
+        MAX_RATIO = 45
         SHORTEST_BOARDER = 60
         result = False
         x_list = [dot[0] for dot in dot_list]
@@ -105,9 +100,6 @@ class OCRHandle(object):
     def get_center(rect: list) -> tuple:
         return (round((rect[0][0] + rect[2][0]) / 2), round((rect[0][1] + rect[2][1]) / 2))
 
-    def get_distance(self, target: tuple, base : tuple = None):
-        return (round(target[0] - self.width / 2), round(target[1] - self.height / 2))
-
     @staticmethod
     def draw_box(img, dot_list: list) -> None:
         dots = [tuple(dot) for dot in dot_list]
@@ -115,75 +107,93 @@ class OCRHandle(object):
         cv2.line(img, dots[1], dots[2], 200, 5)
         cv2.line(img, dots[2], dots[3], 200, 5)
         cv2.line(img, dots[3], dots[0], 200, 5)
-    
+
     @staticmethod
     def contour_to_rect(contour):
         x, y, w, h = cv2.boundingRect(contour)
         return [(x, y), (x + w, y), (x, y + h), (x + w, y + h)]
+
+    def sweap_map(self, img_bin):
+        MARGIN_BUTTOM = 100
+        THRESHHOLD_GRAY_BLUR = 200
+        flood_mask = np.zeros((self.camera_height + 2, self.camera_width + 2), np.uint8)
+
+        line_1_start = (MARGIN_BUTTOM, self.camera_height - 1)
+        line_1_end = (round(0.1 * self.camera_width - 1), 0)
+        line_2_start = (self.camera_width - MARGIN_BUTTOM, self.camera_height - 1)
+        line_2_end = (round(0.9 * self.camera_width - 1), 0)
+        cv2.line(img_bin, line_1_start, line_1_end, 255, 20)
+        cv2.line(img_bin, line_2_start, line_2_end, 255, 20)
+
+        self.videos['video-orig'] = img_bin.copy()
+
+        cv2.floodFill(img_bin, flood_mask, (MARGIN_BUTTOM, self.camera_height - 1), 0)
+        cv2.floodFill(img_bin, flood_mask, (self.camera_width - MARGIN_BUTTOM, self.camera_height - 1), 0)
+        img_bin_blur = cv2.blur(img_bin, (5, 5))
+        retval, img_blur_bin = cv2.threshold(img_bin_blur, THRESHHOLD_GRAY_BLUR, 255, cv2.THRESH_BINARY)
+        return img_blur_bin
+
+    def cut_main_area(self, img):
+        MIN_HEIGHT = 300
+        MAX_HEIGHT = 1000
+        main_area = img[MIN_HEIGHT:MAX_HEIGHT, :]
+        self.videos['video-cut'] = main_area
+        return main_area
+    
+    def find_center(self, main_area, num_rects):
+        main_height = main_area.shape[0]
+        main_width = main_area.shape[1]
+        centers = list(map(self.get_center, num_rects))
+        text_center = (round((centers[0][0] + centers[1][0]) / 2), round((centers[0][1] + centers[1][1]) / 2))
+        center_diff = (round((text_center[0] - main_width * 0.5)), round((text_center[1] - main_height * 0.5)))
+        self.status['x'] = int(center_diff[0])
+        self.status['y'] = int(center_diff[1])
+    
+    @staticmethod
+    def cut_rectangle(img, dot_list: list):
+        x_min, y_min = dot_list[0]
+        x_max, y_max = dot_list[2]
+        return img[y_min:y_max, x_min:x_max]
 
     def analyse_img(self, orig):
         self.status = {}
         self.videos = {}
         self.index += 1
 
-        THRESHHOLD_GRAY_MAIN = 150
-        THRESHHOLD_GRAY_BLUR = 200
-        MIN_HEIGHT = 300
-        MAX_HEIGHT = 1000
-        MARGIN_BUTTOM = 100
+        THRESHHOLD_GRAY_MAIN = 180
 
         width = orig.shape[1]
-        self.width = width
+        self.camera_width = width
         height = orig.shape[0]
-        self.height = height
-        flood_mask = np.zeros((height + 2, width + 2), np.uint8)
+        self.camera_height = height
 
         # canvas = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
         gray = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
         retval, img_bin = cv2.threshold(gray, THRESHHOLD_GRAY_MAIN, 255, cv2.THRESH_BINARY)
-        
-        line_1_start = (MARGIN_BUTTOM, height - 1)
-        line_1_end = (round(0.1 * width - 1), 0)
-        line_2_start = (width - MARGIN_BUTTOM, height - 1)
-        line_2_end = (round(0.9 * width - 1), 0)
-        cv2.line(img_bin, line_1_start, line_1_end, 255, 20)
-        cv2.line(img_bin, line_2_start, line_2_end, 255, 20)
-        self.videos['video-orig'] = img_bin.copy()
-        
-        cv2.floodFill(img_bin, flood_mask, (MARGIN_BUTTOM, height - 1), 0)
-        cv2.floodFill(img_bin, flood_mask, (width - MARGIN_BUTTOM, height - 1), 0)
-        img_bin_blur = cv2.blur(img_bin, (5, 5))
-        retval, img_blur_bin = cv2.threshold(img_bin_blur, THRESHHOLD_GRAY_BLUR, 255, cv2.THRESH_BINARY)
+        img_blur_bin = self.sweap_map(img_bin)
+        main_area = self.cut_main_area(img_blur_bin)
 
-        main_area = img_blur_bin[MIN_HEIGHT:MAX_HEIGHT, :]
-        main_height = main_area.shape[0]
-        main_width = main_area.shape[1]
         _main_area, contours, _hierarchy = cv2.findContours(main_area, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
-        contours = sorted(contours, key=lambda cont: cv2.contourArea(cont), reverse=True)
-        self.videos['video-cut'] = main_area
-
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
         num_rects = list(filter(self.is_rect_valid, [self.contour_to_rect(cont) for cont in contours]))[:2]
         if len(num_rects) == 2:
             num_rects = list(map(self.order_points, num_rects))
             num_rects.sort(key=lambda num_rect: num_rect[0][0])
-            centers = list(map(self.get_center, num_rects))
+            for num_rect in num_rects:
+                self.draw_box(self.videos['video-cut'], num_rect)
+            self.find_center(main_area, num_rects)
             # logging.info(f"num_rects: {num_rects}")
             # logging.info(f"centers: {centers}")
-            self.draw_box(self.videos['video-cut'], num_rects[0])
-            self.draw_box(self.videos['video-cut'], num_rects[1])
 
-            text_center = (round((centers[0][0] + centers[1][0]) / 2), round((centers[0][1] + centers[1][1]) / 2))
-            center_diff = (round((text_center[0] - main_width * 0.5)), round((text_center[1] - main_height * 0.5)))
-            self.status['x'] = int(center_diff[0])
-            self.status['y'] = int(center_diff[1])
+            # num_imgs = []
+            # for num_rect in num_rects:
+            #     box_f = np.float32(num_rect)
+            #     canvas = np.float32([[0, 0], [main_width, 0], [main_width, main_height], [0, main_height]])
+            #     M = cv2.getPerspectiveTransform(box_f, canvas)
+            #     num_imgs.append(cv2.warpPerspective(main_area, M, (0, 0)))
+            num_imgs = [self.cut_rectangle(main_area, num_rect) for num_rect in num_rects]
 
-            num_imgs = []
-            for num_rect in num_rects:
-                box_f = np.float32(num_rect)
-                canvas = np.float32([[0, 0], [main_width,0], [main_width, main_height], [0, main_height]])
-                M = cv2.getPerspectiveTransform(box_f, canvas)
-                num_imgs.append(cv2.warpPerspective(main_area, M, (0, 0)))
-            
-            self.videos['video-nums'] = np.hstack((num_imgs[0], num_imgs[1]))
             self.status['text'] = "".join([self.recognize_number(num_img) for num_img in num_imgs])
+            self.videos['video-num-l'] = num_imgs[0]
+            self.videos['video-num-r'] = num_imgs[1]
             logging.info(f"Result: {self.status}")
