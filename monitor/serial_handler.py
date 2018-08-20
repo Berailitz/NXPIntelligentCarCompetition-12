2e97f3c76d19165f7fe4a92896611914fdab9920
graphite <admin@ohhere.xyz>
2018-08-20 15:22:59 +0800

    Improve serial: always sned data;

--- monitor/serial_handler.py
+++ monitor/serial_handler.py
@@ -12,23 +12,21 @@ class SerialHandler(Process):
         self.queues = queues
 
     def has_task(self):
-        return False
+        return True
 
     def run(self):
         logging.warning("Open serial port `{}` at baudrate `{}`.".format(SERIAL_PORT, SERIAL_BAUDRATE))
-        self.ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE)
-
-        while True:
-            time.sleep(0.05)
-            if self.has_task():
-                self.queues['task_queue'].put(-1)
-            while not self.queues['bytes_queue'].empty():
-                new_bytes = self.queues['bytes_queue'].get()
-                if new_bytes is None:
-                    break
-                else:
-                    if IS_SERIAL_ENABLED:
+        if IS_SERIAL_ENABLED:
+            self.ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE)
+            while True:
+                time.sleep(0.05)
+                if self.has_task():
+                    self.queues['task_queue'].put(-1)
+                while not self.queues['bytes_queue'].empty():
+                    new_bytes = self.queues['bytes_queue'].get()
+                    if new_bytes is None:
+                        break
+                    else:
                         self.ser.write(new_bytes)
-
-        logging.warning("Closing serial port `{}`.".format(SERIAL_PORT))
-        self.ser.close()
+            logging.warning("Closing serial port `{}`.".format(SERIAL_PORT))
+            self.ser.close()
